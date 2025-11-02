3fe98ceecffc38a45528df18141101dfba7dcb4e
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2025-11-02 23:33:52 +0600

    Add files via upload

---
+++ Very_Easy_01.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m,a;
+    cin>>n>>m>>a;
+    int onest=n*m;
+    int secound=a*a;
+    int result =onest-secound;
+    cout<<result;
+
+}
